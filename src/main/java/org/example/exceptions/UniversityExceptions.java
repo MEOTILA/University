713cf4662e1459dd218cac2147ecc094fa9515bc
Sattar <sattargh@aol.com>
2024-12-19 11:26:47 +0330

    added UniversityExceptions Class

---
+++ src/main/java/org/example/exceptions/UniversityExceptions.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class UniversityExceptions extends RuntimeException{
+
+    public UniversityExceptions(String message) {
+        super(message);
+    }
+}
